9dd17fad5063bf385867db97d0f0bbbbcd4ec699
Arthas <145612.364153@internet.ru>
2025-05-07 20:05:49 +0300

    GG WP

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,13 +1,97 @@
 package daysteps
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type DaySteps struct {
 	// TODO: добавить поля
+    Steps    int
+    Duration time.Duration
+    personaldata.Personal
 }
 
 func (ds *DaySteps) Parse(datastring string) (err error) {
 	// TODO: реализовать функцию
+	parts := strings.Split(datastring, ",")
+	if len(parts) != 2 {
+		return errors.New("неверный формат данных")
+	}
+
+	// Обработка шагов с учетом возможного знака '+'
+	stepsInput := parts[0]
+	stepsStr := strings.TrimPrefix(stepsInput, "+")
+	steps, err := strconv.Atoi(stepsStr)
+	if err != nil {
+		return fmt.Errorf("ошибка парсинга шагов: %w", err)
+	}
+
+	// Проверяем соответствие ввода
+	expectedInput := strconv.Itoa(steps)
+	if strings.HasPrefix(stepsInput, "+") {
+		expectedInput = "+" + expectedInput
+	}
+	if expectedInput != stepsInput {
+		return errors.New("некорректный формат шагов")
+	}
+
+	if steps <= 0 {
+		return errors.New("шаги должны быть положительным числом")
+	}
+	ds.Steps = steps
+
+	// Парсинг длительности
+	durationStr := parts[1]
+	duration, err := time.ParseDuration(durationStr)
+	if err != nil {
+		return fmt.Errorf("ошибка парсинга времени: %w", err)
+	}
+	if duration <= 0 {
+		return errors.New("длительность должна быть положительной")
+	}
+	ds.Duration = duration
+
+	return nil
 }
 
 func (ds DaySteps) ActionInfo() (string, error) {
 	// TODO: реализовать функцию
+	if ds.Steps <= 0 {
+		return "", errors.New("количество шагов должно быть положительным")
+	}
+	if ds.Duration <= 0 {
+		return "", errors.New("длительность должна быть положительной")
+	}
+	if ds.Weight <= 0 {
+		return "", errors.New("вес должен быть положительным")
+	}
+	if ds.Height <= 0 {
+		return "", errors.New("рост должен быть положительным")
+	}
+
+	// Расчет показателей
+	distance := spentenergy.Distance(ds.Steps, ds.Height)
+	calories, err := spentenergy.WalkingSpentCalories(
+		ds.Steps,
+		ds.Weight,
+		ds.Height,
+		ds.Duration,
+	)
+	if err != nil {
+		return "", fmt.Errorf("ошибка расчета калорий: %w", err)
+	}
+
+	return fmt.Sprintf(
+		"Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n",
+		ds.Steps,
+		distance,
+		calories,
+	), nil
 }
--- internal/trainings/trainings.go
+++ internal/trainings/trainings.go
@@ -1,13 +1,82 @@
 package trainings
 
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/personaldata"
+	"github.com/Yandex-Practicum/tracker/internal/spentenergy"
+)
+
 type Training struct {
 	// TODO: добавить поля
+    Steps         int
+    TrainingType  string
+    Duration      time.Duration
+    personaldata.Personal
 }
 
 func (t *Training) Parse(datastring string) (err error) {
 	// TODO: реализовать функцию
+	parts := strings.Split(datastring, ",")
+	if len(parts) != 3 {
+		return errors.New("неверный формат данных")
+	}
+
+	// Парсинг и валидация шагов
+	steps, err := strconv.Atoi(strings.TrimSpace(parts[0]))
+	if err != nil || steps <= 0 {
+		return errors.New("некорректное количество шагов")
+	}
+	t.Steps = steps
+
+	// Парсинг типа тренировки
+	t.TrainingType = strings.TrimSpace(parts[1])
+
+	// Парсинг и валидация длительности
+	duration, err := time.ParseDuration(strings.TrimSpace(parts[2]))
+	if err != nil || duration <= 0 {
+		return errors.New("некорректная длительность")
+	}
+	t.Duration = duration
+
+	return nil
 }
 
 func (t Training) ActionInfo() (string, error) {
 	// TODO: реализовать функцию
+	if t.TrainingType != "Бег" && t.TrainingType != "Ходьба" {
+		return "", errors.New("неизвестный тип тренировки")
+	}
+
+	// Расчет показателей
+	distance := spentenergy.Distance(t.Steps, t.Height)
+	speed := spentenergy.MeanSpeed(t.Steps, t.Height, t.Duration)
+
+	var calories float64
+	var err error
+
+	switch t.TrainingType {
+	case "Бег":
+		calories, err = spentenergy.RunningSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+	case "Ходьба":
+		calories, err = spentenergy.WalkingSpentCalories(t.Steps, t.Weight, t.Height, t.Duration)
+	}
+
+	if err != nil {
+		return "", fmt.Errorf("ошибка расчета калорий: %w", err)
+	}
+
+	// Форматирование с завершающим \n
+	return fmt.Sprintf(
+		"Тип тренировки: %s\nДлительность: %.2f ч.\nДистанция: %.2f км.\nСкорость: %.2f км/ч\nСожгли калорий: %.2f\n",
+		t.TrainingType,
+		t.Duration.Hours(),
+		distance,
+		speed,
+		calories,
+	), nil
 }
